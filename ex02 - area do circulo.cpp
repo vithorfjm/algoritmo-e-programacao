dc0aa80a284180f4c30110801946b793b92e6190
vithorfjm <91703672+vithorfjm@users.noreply.github.com>
2022-02-16 11:15:41 -0300

    Constante e biblioteca math

--- ex02 - area do circulo.cpp
+++ ex02 - area do circulo.cpp
@@ -1,5 +1,5 @@
 /* A fórmula para calcular a área de uma circunferência é: area = pi . raio2.
-Considerando para este problema que pi = 3.14159:
+Considerando para este problema que pi = 3.14159: 
 - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por pi. */
 
 #include <stdio.h>
